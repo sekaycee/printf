2506137800cf94d15a8351fe6fcee3ebf682406d
Jo Kar <josi.kelechi@gmail.com>
2022-10-17 09:39:37 -0700

    Add main functions to main.h

--- main.h
+++ main.h
@@ -20,15 +20,58 @@
 #define S_SHORT 1
 
 /**
- * struct fmt - struct op
- * @fmt: print format
+ * struct print - struct op
+ * @format: print format
  * @fn: synonymous function
  */
-struct fmt
+struct print
 {
 	char format;
 	int (*fn)(va_list, char[], int, int, int, int);
-} fmt_t;
+} print_t;
+
+int _printf(const char *format, ...);
+int handle_print(const char *, int *, va_list, char *, int, int, int, int);
+
+/* functions to print chars and strings */
+int print_char(va_list, char *, int, int, int, int);
+int print_string(va_list, char *, int, int, int, int);
+int print_percent(va_list, char *, int, int, int, int);
+
+/* functions to print numbers */
+int print_int(va_list, char *, int, int, int, int);
+int print_bin(va_list, char *, int, int, int, int);
+int print_unsgnd(va_list, char *, int, int, int, int);
+int print_oct(va_list, char *, int, int, int, int);
+int print_hex(va_list, char *, char *, int, char, int, int, int);
+int print_uhex(va_list, char *, int, int, int, int);
+int print_hex_upper(va_list, char *, int, int, int, int);
+
+/* function to print non-printable characters */
+int print_non_printable(va_list, char *, int, int, int, int);
+
+/* function to print memory address */
+int print_pointer(va_list, char *, int, int, int, int);
+
+/* functions to handle other specifiers */
+int get_flags(const char *, int *);
+int get_width(const char *, int *, va_list);
+int get_precision(const char *, int *, va_list);
+int get_size(const char *, int *);
+
+/* function to print string in reverse */
+int print_reverse(va_list, char *, int, int, int, int);
+
+/* function to print a string in rot 13 */
+int print_rot13(va_list, char *, int, int, int, int);
+
+/* write handlers */
+int write_char_handler(char c, char *, int, int, int, int);
+int write_num(int, int, char *, int, int, int, int);
+int write_num_helper(int, char *, int, int, int, int, char, char);
+int write_pointer(char *, int, int, int, int, char, char, int);
+int write_unsgnd(int, int, char *, int, int, int, int);
+
 
 #endif /* MAIN_H */
 
