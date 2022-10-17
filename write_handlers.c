3e0f1adecfba2d95c269d5f3ba0437b4dc2996f7
Jo Kar <josi.kelechi@gmail.com>
2022-10-17 12:06:03 -0700

    Write num helper function

--- write_handlers.c
+++ write_handlers.c
@@ -3,16 +3,16 @@
 /**
  * write_char_handler - print a string
  * @c: char types
- * @b: buffer to handle print
- * @f: computes active flags
- * @w: get width
+ * @b: b to handle print
+ * @f: computes active f
+ * @w: get w
  * @p: precision specifier
  * @s: size specifier
  * Return: number of chars printed
  */
 int write_char_handler(char c, char b[],
 	int f, int w, int p, int s)
-{ /* char is stored at left and padding at b's right */
+{ /* char is stored at left and pading at b's right */
 	int i = 0;
 	char pad = ' ';
 
@@ -45,10 +45,10 @@ int write_char_handler(char c, char b[],
 /**
  * write_num - print a number to stdout
  * @neg: list of arguments
- * @n: buffer limit
- * @b: buffer to handle print
- * @f:  compute active flags
- * @w: get width
+ * @n: b limit
+ * @b: b to handle print
+ * @f:  compute active f
+ * @w: get w
  * @p: precision specifier
  * @s: size specifier
  * Return: number of chars printed
@@ -70,6 +70,65 @@ int write_num(int neg, int n, char *b, int f, int w, int p, int s)
 		xc = ' ';
 
 	return (write_num_helper(n, b, f, w, p, l, pad, xc));
+}
+
+/**
+ * write_num - Write a number using a bufffer
+ * @n: Index at which the number starts on the b
+ * @b: Buffer
+ * @f: Flags
+ * @w: w specifier
+ * @p: Precision specifier
+ * @l: Number l
+ * @pad: Pading char
+ * @xc: Extra char
+ *
+ * Return: Number of printed chars.
+ */
+int write_num(int n, char b[],
+	int f, int w, int prec,
+	int l, char pad, char xc)
+{
+	int i, ps = 1;
+
+	if (p == 0 && n == BUFF_SIZE - 2 && b[n] == '0' && w == 0)
+		return (0); /* printf(".0d", 0)  no char is printed */
+	if (p == 0 && n == BUFF_SIZE - 2 && b[n] == '0')
+		b[n] = pad = ' '; /* w is displayed with pading ' ' */
+	if (p > 0 && p < l)
+		pad = ' ';
+	while (p > l)
+		b[--n] = '0', l++;
+	if (xc != 0)
+		l++;
+	if (w > l)
+	{
+		for (i = 1; i < w - l + 1; i++)
+			b[i] = pad;
+		b[i] = '\0';
+		if (f & F_MINUS && pad == ' ')/* Asign extra char to left of b */
+		{
+			if (xc)
+				b[--n] = xc;
+			return (write(1, &b[n], l) + write(1, &b[1], i - 1));
+		}
+		else if (!(f & F_MINUS) && pad == ' ')/* extra char to left of buff */
+		{
+			if (xc)
+				b[--n] = xc;
+			return (write(1, &b[1], i - 1) + write(1, &b[n], l));
+		}
+		else if (!(f & F_MINUS) && pad == '0')/* extra char to left of pad */
+		{
+			if (xc)
+				b[--ps] = xc;
+			return (write(1, &b[ps], i - ps) +
+				write(1, &b[n], l - (1 - ps)));
+		}
+	}
+	if (xc)
+		b[--n] = xc;
+	return (write(1, &b[n], l));
 }
 
 
